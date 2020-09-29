468408ac3915a0d171eba598c0f91937492a4325
git-bugs <voov3r@gmail.com>
2020-09-29 10:54:57 +0300

    index

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,40 @@
-let x = document.querySelector('button');
-x.addEventListener('click', function(e){
-    console.log(e.target);
-})
+$(function(){
+    $('.products__inner').slick({
+        arrows: true, 
+        dots: false,
+        prevArrow: '<button type="button" class="slick-prev lnr lnr-chevron-left"></button>',
+        nextArrow: '<button type="button" class="slick-next lnr lnr-chevron-right"></button>'
+    });
+
+    $('.followers__items-inner').slick({
+        arrows: true, 
+        dots: false,
+        slidesToShow: 3,
+        variableWidth: true,
+        prevArrow: '<button type="button" class="slick-prev lnr lnr-chevron-left"></button>',
+        nextArrow: '<button type="button" class="slick-next lnr lnr-chevron-right"></button>'
+    });
+
+    $('.item-rate').rateYo({
+        rating: 4.5,
+        starWidth: "17px",
+        readOnly: true
+    });
+
+    $('.category__item-rate').rateYo({
+        rating: 4,
+        starWidth: "15px",
+        readOnly: true
+    });
+    
+
+
+
+
+    
+
+
+
+
+    var mixer = mixitup('.category__items');
+});
--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,8 @@ gulp.task('sass',function(){
 gulp.task('script', function(){
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
-        'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+        'node_modules/rateyo/src/jquery.rateyo.js',
+        'node_modules/mixitup/dist/mixitup.min.js'
     ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
@@ -30,9 +31,9 @@ gulp.task('script', function(){
 
 gulp.task('style', function(){
     return gulp.src([
-        'node_modules/normalize.css/normalize.css',
+        'node_modules/reset.css/reset.css',
         'node_modules/slick-carousel/slick/slick.css',
-        'node_modules/magnific-popup/dist/magnific-popup.css'
+        'node_modules/rateyo/src/jquery.rateyo.css'
     ])
     .pipe(concat('libs.min.css'))
     .pipe(cssmin())
